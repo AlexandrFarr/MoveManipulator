f5c57b720cfdfc9135d3bb38ca2b1c7409012ed7
Alexandr Danilin <101105839+AlexandrFarr@users.noreply.github.com>
2022-04-12 11:11:47 +0300

    Update Point.h

--- MoveManipulator/Point.h
+++ MoveManipulator/Point.h
@@ -2,16 +2,22 @@
 class Point
 {
 private:
+
     double x, y;
+
 public:
+
     Point() {
         x = 0;
         y = 0;
     }
+
     Point(double _x, double _y) {
         x = _x;
         y = _y;
     }
+
+    virtual ~Point() {}
 
     double get_x();
     double get_y();
